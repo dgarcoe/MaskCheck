c3e5036fff6bf7f597fcfb1e872a78e5755da3eb
dgarcoe <dannn86m@gmail.com>
2020-08-15 22:39:02 +0200

    Modified layouts

---
+++ app/src/main/java/com/github/dgarcoe/maskchecker/ReferencesActivity.kt
@@ -0,0 +1,14 @@
+package com.github.dgarcoe.maskchecker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ReferencesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_references)
+
+
+    }
+}
